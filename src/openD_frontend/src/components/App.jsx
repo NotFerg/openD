2d85e552dc17119e3b2ede6004f57556b84a77ab
Fergus <fergus.ampuan@gmail.com>
2024-07-17 21:51:47 +0800

    can now view NFT on the front end

--- src/openD_frontend/src/components/App.jsx
+++ src/openD_frontend/src/components/App.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
-import homeImage from "../../public/home-img.png";
+import homeImage from "/home-img.png";
+import Item from "./Item";
 
 function App() {
+
+  const NFTID = "bkyz2-fmaaa-aaaaa-qaaaq-cai";
   return (
     <div className="App">
       <Header />
-      <img className="bottom-space" src={homeImage} />
+      <Item id={NFTID}/>
+      {/* <img className="bottom-space" src={homeImage} /> */}
       <Footer />
     </div>
   );
